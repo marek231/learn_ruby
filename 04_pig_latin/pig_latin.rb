5995c2c86ae11e87a8bf7cfd1e7ee20ed1de0674
Marek <pruteanupopescumarek@gmail.com>
2019-05-17 01:01:06 +0300

    Simplify logic

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,12 @@
 class String
   def pigify
-    self.split(/ /).map do |word|
+    split(/ /).map do |word|
       if /^[aeiouy]/.match(word)
         word << 'ay'
       else
         # Capture as many consonants at the start of the string and an optional 'u'.
-        starting_consonants = /^[^aeiouy]*u?/.match(word).to_s
-        word = word[starting_consonants.size..-1] << starting_consonants << 'ay'
+        starting_consonants = word.slice!(/^[^aeiouy]*u?/)
+        word << starting_consonants << 'ay'
       end
     end.join(' ')
   end
