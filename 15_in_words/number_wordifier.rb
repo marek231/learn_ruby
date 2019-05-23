9c4ff9aa1859aacdb615c3aa3d7edccbb4d3c20f
Marek <pruteanupopescumarek@gmail.com>
2019-05-23 03:11:55 +0300

    Refactor parsing numbers larger than 3 digits

--- 15_in_words/number_wordifier.rb
+++ 15_in_words/number_wordifier.rb
@@ -1,14 +1,14 @@
 class NumberWordifier
-  PRE_TWENTY = [
+  UNDER_TWENTY = [
     '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
     'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
     'seventeen', 'eighteen', 'nineteen'
   ].freeze
-  TENS = [
+  MULTIPLE_OF_TEN = [
     '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
     'seventy', 'eighty', 'ninety'
   ].freeze
-  POWERS_OF_THOUSAND = ['', 'thousand'] + %w[
+  POWER_OF_ONE_THOUSAND = ['', 'thousand'] + %w[
     m b tr quadr quint sext sept oct non dec
   ].map { |prefix| "#{prefix}illion" }.freeze
 
@@ -23,31 +23,27 @@ class NumberWordifier
   def parse(number)
     case number
     when 0..19
-      PRE_TWENTY[number]
+      UNDER_TWENTY[number]
     when 20..99
-      two_digits(number).rstrip
+      parse_two_digits(number).rstrip
     when 100..999
-      "#{three_digits(number)} #{parse(number % 100)}".rstrip
+      "#{parse_three_digits(number)} #{parse(number % 100)}".rstrip
     else
-      more_than_three_digits(number).rstrip
+      parse_more_than_three_digits(number).rstrip
     end
   end
 
-  def two_digits(number)
-    "#{TENS[number / 10]} #{PRE_TWENTY[number % 10]}"
+  def parse_two_digits(number)
+    "#{MULTIPLE_OF_TEN[number / 10]} #{UNDER_TWENTY[number % 10]}"
   end
 
-  def three_digits(number)
-    "#{PRE_TWENTY[number / 100]} hundred"
+  def parse_three_digits(number)
+    "#{UNDER_TWENTY[number / 100]} hundred"
   end
 
-  def more_than_three_digits(number)
-    chunks = []
-    until number.zero?
-      number, remainder = number.divmod(1000)
-      chunks << remainder
-    end
-    chunks.map { |chunk| parse chunk }.zip(POWERS_OF_THOUSAND).reverse
-          .reject { |c| c.first.empty? }.join(' ')
+  def parse_more_than_three_digits(number)
+    number.digits.each_slice(3).map { |g| g.join.reverse.to_i }
+          .map { |g| parse g }.zip(POWER_OF_ONE_THOUSAND).reverse
+          .reject { |g| g.first.empty? }.join(' ')
   end
 end
